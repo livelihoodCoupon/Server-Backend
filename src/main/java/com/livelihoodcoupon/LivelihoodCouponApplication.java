681689d1ef36506a7a8f15a316599359b007dca9
meraki6512 <jweun123@gmail.com>
2025-09-16 09:31:58 +0900

    Initial commit

---
+++ src/main/java/com/livelihoodcoupon/LivelihoodCouponApplication.java
@@ -0,0 +1,13 @@
+package com.livelihoodcoupon;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LivelihoodCouponApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(LivelihoodCouponApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/livelihoodcoupon/LivelihoodCouponApplicationTests.java
@@ -0,0 +1,13 @@
+package com.livelihoodcoupon;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LivelihoodCouponApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}

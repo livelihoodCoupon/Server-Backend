dfc472928abc9f7b6dc20c35956b8674be944770
sungchae <sungge0661@naver.com>
2025-10-10 17:03:04 +0900

    refactor: 최종주차장csv -> db 적재를 위한 코드리팩

--- src/main/java/com/livelihoodcoupon/batch/ParkingLotCsvBatchConfig.java
+++ src/main/java/com/livelihoodcoupon/batch/ParkingLotCsvBatchConfig.java
@@ -47,14 +47,15 @@ public class ParkingLotCsvBatchConfig {
     private static final int CHUNK_SIZE = 1000;
 
     private static final String[] COLUMN_NAMES = new String[] {
-        "주차장관리번호", "주차장명", "주차장구분", "주차장유형", "소재지도로명주소",
-        "소재지지번주소", "주차구획수", "급지구분", "부제시행구분", "운영요일",
-        "평일운영시작시각", "평일운영종료시각", "토요일운영시작시각", "토요일운영종료시각",
-        "공휴일운영시작시각", "공휴일운영종료시각", "요금정보", "주차기본시간", "주차기본요금",
-        "추가단위시간", "추가단위요금", "1일주차권요금적용시간", "1일주차권요금", "월정기권요금",
-        "결제방법", "특기사항", "관리기관명", "전화번호", "위도", "경도",
-        "장애인전용주차구역보유여부", "데이터기준일자", "제공기관코드", "제공기관명"
+        "parkingLotNo", "parkingLotNm", "parkingLotSe", "parkingLotType",
+        "roadAddress", "lotAddress", "parkingCapacity", "feedingSe", "enforceSe", "operDay",
+        "weekOpenTime", "weekCloseTime", "satOpenTime", "satCloseTime", "holidayOpenTime",
+        "holidayCloseTime", "parkingChargeInfo", "basicTime", "basicCharge", "addUnitTime",
+        "addUnitCharge", "dayTicketApplyTime", "dayTicketCharge",
+        "paymentMethod", "specialComment", "institutionName", "phoneNumber", "lat", "lng",
+        "disabledParkingZoneYn", "referenceDate", "institutionCode"
     };
+
 
     @Bean
     public Job parkingLotCsvJob() {
@@ -172,7 +173,7 @@ public class ParkingLotCsvBatchConfig {
                 .specialComment(item.getSpecialComment())
                 .institutionName(item.getInstitutionName())
                 .phoneNumber(item.getPhoneNumber())
-                .location(point) // 좌표 실패 시 null
+                .location(point)
                 .disabledParkingZoneYn(item.getDisabledParkingZoneYn())
                 .referenceDate(item.getReferenceDate())
                 .institutionCode(item.getInstitutionCode())
